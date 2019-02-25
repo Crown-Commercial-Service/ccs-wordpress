044ca73e7fb1739dd01ac0c26ee1df0db88f14d5
Sean Dunwoody <sean.dunwoody@studio24.net>
2019-02-25 17:36:46 +0000

    Allow REST API access without being logged in

--- public/wp-content/plugins/ccs-custom/ccs-custom.php
+++ public/wp-content/plugins/ccs-custom/ccs-custom.php
@@ -6,7 +6,7 @@ Description: A WordPress plugin to contain custom code for the CCS website
 Version: 0.1
 */
 
-include('library/rest-api.php');
+//include('library/rest-api.php');
 include('library/editor-customisation.php');
 include('library/custom-taxonomies.php');
 include('library/custom-post-types.php');

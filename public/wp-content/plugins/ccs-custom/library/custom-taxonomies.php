47ad38553c1b559ca682cbf493071e62be1d12d7
Sean Dunwoody <sean.dunwoody@studio24.net>
2019-03-19 15:21:05 +0000

    [CCS3-436] Change the “Framework Type” taxonomy so that users have to select a value for it

--- public/wp-content/plugins/ccs-custom/library/custom-taxonomies.php
+++ public/wp-content/plugins/ccs-custom/library/custom-taxonomies.php
@@ -5,3 +5,9 @@ function ccs_register_my_taxonomies() {
     register_taxonomy( 'framework_type', array( 'framework' ), array('hierarchical' => true, 'label' => 'Framework Type', 'capabilities' => ['assign_terms' => 'edit_frameworks'], 'show_in_rest' => true ));
     register_taxonomy( 'framework_status', array( 'framework' ), array('hierarchical' => true, 'label' => 'Framework Status', 'capabilities' => ['assign_terms' => 'edit_frameworks'], 'show_in_rest' => true ));
 }
+
+/**
+ * Make the framework_type taxonomy required (don't allow users to select
+ * "No Framework Type" as a choice)
+ */
+add_filter( "radio_buttons_for_taxonomies_no_term_framework_type", "__return_FALSE" );

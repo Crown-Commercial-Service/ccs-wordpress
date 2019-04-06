c0f838336aa8d1fdb10191eea20296e729e82dd3
Dan Harris <dan@sparkcode.co.uk>
2019-04-06 17:12:58 +0100

    * Initial import server management code * Relaxed stop/start script timeouts

--- deploy/server_stop.sh
+++ deploy/server_stop.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 # Stop application-related services
 
+WP_IMPORT_SCRIPT="/home/ec2-user/wp_import.sh"
+WP_IMPORT_PID_FILE="/home/ec2-user/wp_import_sh.pid"
+
 echo "Starting codedeploy server_stop.sh ..."
 
-sudo systemctl is-active --quiet httpd \
-    && sudo systemctl stop httpd.service
+# Is an import server
+if [ -f "$WP_IMPORT_SCRIPT" ]; then
+    # Import in progress
+    if [ -f "$WP_IMPORT_PID_FILE" ]; then
+        echo "Active import detected, stopping..."
+        kill $(cat "$WP_IMPORT_PID_FILE")
+        echo "Import stopped."
+    fi
+else
+    sudo systemctl is-active --quiet httpd \
+        && sudo systemctl stop httpd.service
+fi
 
 echo "Codedeploy server_stop.sh complete."

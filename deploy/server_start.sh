571f73d4a4f031609310b5ddef5aecda596f988d
Dan Harris <dan@sparkcode.co.uk>
2019-10-30 13:27:26 +0000

    * Initial code

--- deploy/server_start.sh
+++ deploy/server_start.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 # Start/enable application-related services
 
+IMPORT_APP_NAME="deploy-web_import"
 WP_IMPORT_SCRIPT="/home/ec2-user/wp_import.sh"
 
 echo "Starting codedeploy server_start.sh ..."
 
-# Not an import server
-if [ ! -f "$WP_IMPORT_SCRIPT" ]; then
-    sudo systemctl is-enabled --quiet httpd \
-        || sudo systemctl enable httpd
+echo -n "> Detecting deployment type: "
+if [ "$APPLICATION_NAME" == "$IMPORT_APP_NAME" ]; then
+    echo "import."
 
-    sudo systemctl is-active --quiet httpd \
-        || sudo systemctl start httpd
+    SERVICES=(
+        "awslogsd.service"
+    )
+else
+    echo "cms."
+
+    SERVICES=(
+        "awslogsd.service"
+        "httpd.service"
+    )
 fi
+
+echo "> Starting services..."
+for SERVICE in "${SERVICES[@]}"; do
+    echo -n "> > Enabling & starting service [$SERVICE]: "
+
+    sudo systemctl is-enabled --quiet "$SERVICE" \
+        || sudo systemctl enable "$SERVICE"
+
+    sudo systemctl is-active --quiet "$SERVICE" \
+        || sudo systemctl start "$SERVICE"
+
+    echo "done."
+done
 
 echo "Codedeploy server_start.sh complete."

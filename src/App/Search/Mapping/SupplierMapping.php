fc92b93937f75fc00ae5f4ec70b64805fb674870
Matt Buckland <matt.b@studio24.net>
2019-12-05 13:16:52 +0000

    Removed the custom analyzer from the supplier search framework title to avoid errors

--- src/App/Search/Mapping/SupplierMapping.php
+++ src/App/Search/Mapping/SupplierMapping.php
@@ -28,7 +28,7 @@ class SupplierMapping extends AbstractMapping
         'type'       => 'nested',
         'properties' => [
           'end_date'            => ['type' => 'date'],
-          'title'               => ['type' => 'keyword', 'analyzer' => 'english_analyzer'],
+          'title'               => ['type' => 'keyword'],
           'rm_number'           => ['type' => 'text', 'fielddata' =>'true'],
           'rm_number_numerical' => ['type' => 'keyword'],
           'status'              => ['type' => 'keyword'],

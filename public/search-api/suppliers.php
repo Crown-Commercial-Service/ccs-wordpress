495cb1abeb9cb845cc914757ba682084ac6d771d
Matt Buckland <matt.b@studio24.net>
2019-12-09 11:57:56 +0000

    Cleaned up the supplier api code slightly

--- public/search-api/suppliers.php
+++ public/search-api/suppliers.php
@@ -15,22 +15,24 @@ $dotenv->load($rootDir . '.env');
 $searchClient = new SupplierSearchClient();
 $facetDataResolver = new FacetDataResolver();
 
+
+// Set empty vars
+$filters = [];
+$keyword = '';
+$page = 0;
+$limit = 20;
+
 if (isset($_GET['limit']) && !empty($_GET['limit'])) {
     $limit = (int)filter_var($_GET['limit'], FILTER_SANITIZE_STRING);
 }
-$limit = $limit ?? 20;
 
 if (isset($_GET['page']) && !empty($_GET['page'])) {
     $page = (int)filter_var($_GET['page'], FILTER_SANITIZE_STRING);
 }
-$page = $page ?? 0;
 
-$keyword = '';
 if (isset($_GET['keyword']) && !empty($_GET['keyword'])) {
     $keyword = filter_var($_GET['keyword'], FILTER_SANITIZE_STRING);
 }
-
-$filters = [];
 
 if (isset($_GET['framework']) && !empty($_GET['framework'])) {
     $frameworkRmNumber = filter_var($_GET['framework'], FILTER_SANITIZE_STRING);
@@ -51,29 +53,26 @@ if (isset($_GET['lot']) && !empty($_GET['lot'])) {
     ];
 }
 
+// Run the full query we want the results for
 $resultSet = $searchClient->queryByKeyword($keyword, $page, $limit, $filters);
 $suppliers = $resultSet->getResults();
 
-// We then want to do a
-
+// We then want to do a separate query to get the facet results we require
 $facetSearch = $searchClient->queryByKeyword($keyword, $page, $limit);
 $facets = $facetSearch->getAggregations();
+// If the lots facet is set, let's add them in
 if (isset($lots))
 {
     $facets['lots'] = $lots;
 }
 $buckets = $facetDataResolver->prepareFacetsForView($facets);
 
-// If the lots facet is set, let's add them
-
 $supplierDataToReturn = [];
-
 /** @var \Elastica\Result $supplier */
 foreach ($suppliers as $supplier)
 {
     $supplierDataToReturn[] = $supplier->getSource();
 }
-
 
 $meta = [
   'total_results' => $resultSet->getTotalHits(),

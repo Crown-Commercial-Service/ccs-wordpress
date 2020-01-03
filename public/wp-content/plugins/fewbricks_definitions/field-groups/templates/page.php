8ae6080f53e46223c9f85bfdda0d85946a682d91
Carlos Eriksson <carlos@studio24.net>
2020-01-03 10:40:47 +0000

    Add fixed Hero component and flexible components (with test Text component) - WIP

--- public/wp-content/plugins/fewbricks_definitions/field-groups/templates/page.php
+++ public/wp-content/plugins/fewbricks_definitions/field-groups/templates/page.php
@@ -17,7 +17,33 @@ $location = [
 
 
 
-$fg5 = ( new fewacf\field_group( 'Lead Text', '201903111200a', $location, 5, [
+
+/**
+* Define the field group
+ *
+ * Field groups with a lower menu_order will appear first on the edit screens (change by 10,20,30 increments to give yourself space to add)
+ */
+$field_group = ( new fewacf\field_group( 'Hero', '202001031016a', $location, 10, [
+	'position' => 'acf_after_title',
+	'names_of_items_to_hide_on_screen' => [
+		'excerpt'
+	]
+]));
+
+/**
+ * Define the fields
+ */
+$field_group->add_brick((new bricks\component_hero('hero', '202001031016b')) );
+
+/*
+ * Register the field group
+ */
+$field_group->register();
+
+
+
+
+$fg5 = ( new fewacf\field_group( 'Lead Text', '201903111200a', $location, 20, [
     'position' => 'acf_after_title',
     'names_of_items_to_hide_on_screen' => [
     ]
@@ -32,7 +58,7 @@ $fg5->register();
 
 
 
-$fg6 = ( new fewacf\field_group('Accordion', '201903111208a', $location, 10, [
+$fg6 = ( new fewacf\field_group('Accordion', '201903111208a', $location, 30, [
     'names_of_items_to_hide_on_screen' => [
     ]
 ]) );
@@ -46,7 +72,7 @@ $fg6->register();
 
 
 
-$fg8 = ( new fewacf\field_group( 'Full width content', '201903201810a', $location, 5, [
+$fg8 = ( new fewacf\field_group( 'Full width content', '201903201810a', $location, 40, [
     'names_of_items_to_hide_on_screen' => [
     ]
 ] ));
@@ -56,3 +82,24 @@ $fg8->add_field( new acf_fields\wysiwyg( 'Full width content', 'full_width_conte
 ] ) );
 
 $fg8->register();
+
+
+
+/**
+ * Define the field group
+ *
+ * Field groups with a lower menu_order will appear first on the edit screens (change by 10,20,30 increments to give yourself space to add)
+ */
+$field_group = ( new fewacf\field_group( 'Page components', '202001031015a', $location, 50, [
+	'position' => 'normal',
+]));
+
+/**
+ * Define the fields
+ */
+$field_group->add_brick((new bricks\group_page_content_default('page_components', '202001031015b')));
+
+/*
+ * Register the field group
+ */
+$field_group->register();

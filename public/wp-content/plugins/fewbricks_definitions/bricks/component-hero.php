7ea266be2c0a1202d71bcb09bb516d860ea5db71
Carlos Eriksson <carlos@studio24.net>
2020-01-08 13:56:50 +0000

    Add Landing template with provisional components

--- public/wp-content/plugins/fewbricks_definitions/bricks/component-hero.php
+++ public/wp-content/plugins/fewbricks_definitions/bricks/component-hero.php
@@ -22,18 +22,21 @@ class component_hero extends project_brick {
 	 */
 	public function set_fields() {
 
-		//$this->add_brick( new component_image( 'hero_object', '201701311324t' ));
-		$this->add_field( new acf_fields\image( 'Hero Image', 'hero_image', '202001031023a', [
+		$this->add_field( new acf_fields\image( 'Hero Image', 'image', '202001031023a', [
 			'instructions' => 'Leave blank if no hero required.',
 			'min_width' => 1042,
 			'min_height' => 480,
-			'return_format' => 'array'
 		] ) );
 
 		// REVIEW MICROCOPY
 		// Minimum size required is 1042 pixels wide and 480 pixels tall, however the recommended dimensions are 2084 pixels wide and 960 pixels tall (uploading at these larger dimensions will ensure that retina displays receive a high resolution image). Please note that these hero images are vertically cropped on larger screen sizes - so please try and ensure the focal point of the image is vertically in the middle (as the image is cropped vertically on the center)
 
-		$this->add_field( new acf_fields\text( 'Heading', 'hero_heading', '202001031023b' ));
+		$this->add_field( new acf_fields\text( 'Heading', 'heading', '202001031023b' ));
+		$this->add_field( new acf_fields\wysiwyg( 'Content', 'content', '202001031023c' ));
+		$this->add_field( new acf_fields\text( 'CTA label', 'cta_label', '202001031023d' ));
+		$this->add_field( new acf_fields\text( 'CTA destination', 'cta_destination', '20200108105a', [
+			'instructions' => 'Add a # before the destination name to indicate that this is an anchor link, i.e. a link which takes you to a different part of the same page. The destination name ought to be unique to the page and match the name you give your component.'
+		] ));
 
 	}
 
@@ -43,17 +46,12 @@ class component_hero extends project_brick {
 	 */
 	protected function get_brick_html() {
 
-		// We can't use Timber to create an image object anymore so let's set return_format to `array` instead
-//		$imageId = $this->get_field( 'hero_image' );
-//		$data['hero_image'] = '';
-//		if(!empty($imageId)) {
-//			$data['hero_image'] = new \Timber\Image($imageId);
-//			$data['hero_image_width'] = $data['hero_image']->width();
-//		}
-
 		$data = [
-			'hero_heading'  => $this->get_field( 'hero_heading' ),
-			'hero_image'  => $this->get_field( 'hero_image' )
+			'heading'  => $this->get_field( 'heading' ),
+			'image'  => $this->get_field( 'image' ),
+			'content'  => apply_filters( 'the_content' , $this->get_field( 'content' ) ),
+			'cta_label'  => $this->get_field( 'cta_label' ),
+			'cta_destination'  => $this->get_field( 'cta_destination' )
 		];
 
 		return $data;
---
+++ public/wp-content/plugins/fewbricks_definitions/bricks/component-intro.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace fewbricks\bricks;
+
+use fewbricks\acf\fields as acf_fields;
+use fewbricks\acf\layout;
+
+/**
+ * Class text_and_content
+ * @package fewbricks\bricks
+ */
+class component_intro extends project_brick {
+
+	/**
+	 * @var string This will be the default label showing up in the editor area for the administrator.
+	 * It can be overridden by passing an item with the key "label" in the array that is the second argument when
+	 * creating a brick.
+	 */
+	protected $label = 'Intro and description';
+
+	/**
+	 * This is where all the fields for the brick will be set-
+	 */
+	public function set_fields() {
+
+		$this->add_field( new acf_fields\text( 'Heading', 'heading', '202001031433a' ) );
+
+		$this->add_field( new acf_fields\wysiwyg( 'Content', 'content', '202001031433b' ) );
+//		$this->add_field( new acf_fields\text( 'CTA label', 'cta_label', '202001031433d' ));
+
+
+		$fc = new acf_fields\flexible_content( 'Media', 'media', '2020011440a', [
+			'button_label' => 'Add media to section',
+			'layout'       => 'row',
+			'min'          => '',
+			'max'          => 1,
+		] );
+
+		$l = new layout( '', 'image', '2020011440b' );
+		$l->add_brick( new component_image( 'image', '2020011440d' ) );
+		$fc->add_layout( $l );
+
+		$l = new layout( '', 'video', '2020011440c' );
+		$l->add_brick( new component_video( 'video', '2020011440e' ) );
+		$fc->add_layout( $l );
+
+		$this->add_flexible_content( $fc );
+
+	}
+
+	/**
+	 * Function to show what Twig could do for you
+	 * @return array
+	 */
+	protected function get_brick_html() {
+
+		$media = array();
+
+		 while ( $this->have_rows('media') ) {
+			 $this->the_row();
+
+			 array_push( $media ,
+				 array(
+					 'html' => acf_fields\flexible_content::get_sub_field_brick_instance()->get_html()
+				 )
+			 );
+		 }
+
+//		while ( $this->have_rows( 'media' ) ) {
+//			$this->the_row();
+//			$media = $this->get_child_brick_in_repeater( 'media', 'block', 'component_block' )->get_html();
+//			array_push( $media, $data );
+//		}
+
+		$data = [
+			'heading' => $this->get_field( 'heading' ),
+			'content' => apply_filters( 'the_content', $this->get_field( 'content' ) ),
+			'media'   => $media
+		];
+
+		return $data;
+
+	}
+
+}

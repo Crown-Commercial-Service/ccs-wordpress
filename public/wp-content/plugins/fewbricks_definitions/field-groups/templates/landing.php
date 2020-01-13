63840b7de1403e1ba853b629dcba881c52c7332d
Sean Dunwoody <sean.dunwoody@studio24.net>
2020-01-13 10:01:17 +0000

    Add brochures and resources intro fields

--- public/wp-content/plugins/fewbricks_definitions/field-groups/templates/landing.php
+++ public/wp-content/plugins/fewbricks_definitions/field-groups/templates/landing.php
@@ -48,13 +48,35 @@ $field_group->register();
  * Field groups with a lower menu_order will appear first on the edit screens (change by 10,20,30 increments to give yourself space to add)
  */
 $field_group = ( new fewacf\field_group( 'Page components', '202001081046c', $location, 50, [
-	'position' => 'normal',
+    'position' => 'normal',
 ]));
 
 /**
  * Define the fields
  */
 $field_group->add_brick((new bricks\group_page_content_default('page_components', '202001081046d')));
+
+/*
+ * Register the field group
+ */
+$field_group->register();
+
+
+
+/**
+ * Define the field group
+ *
+ * Field groups with a lower menu_order will appear first on the edit screens (change by 10,20,30 increments to give yourself space to add)
+ */
+$field_group = ( new fewacf\field_group( 'Page Resources', '202001101220a', $location, 60, [
+    'position' => 'normal',
+]));
+
+/**
+ * Define the fields
+ */
+$field_group->add_brick((new bricks\resources_intro('resources_intro', '202001101222b')));
+$field_group->add_brick((new bricks\brochures_list('brochures_list', '202001101634a')));
 
 /*
  * Register the field group

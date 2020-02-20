a7ee6d4e9520169dea9c1bae493b35b1a0e36c15
Carlos Eriksson <carlos@studio24.net>
2020-02-20 10:47:50 +0000

    Add acf fields to Page post-types. Fix missing Lead text on Post post-types

--- public/wp-content/plugins/ccs-custom/library/add-acf-fields-to-rest-api.php
+++ public/wp-content/plugins/ccs-custom/library/add-acf-fields-to-rest-api.php
@@ -18,41 +18,33 @@
 ////}, 10, 3 );
 //
 //
-///**
-// * V3
-// */
-///**
-// * Define which post-types/taxonomies should get to pass along their ACF fields
-// *
-// * https://github.com/airesvsg/acf-to-rest-api/issues/109#issuecomment-286403063
-// */
-//add_filter( 'acf/rest_api/page/get_fields', function ( $data ) {
-//	if ( ! empty( $data ) ) {
-//		array_walk_recursive( $data, 'get_fields_recursive' );
-//	}
-//
-//	return $data;
-//} );
-//
-//add_filter( 'acf/rest_api/pillar/get_fields', function ( $data ) {
-//	if ( ! empty( $data ) ) {
-//		array_walk_recursive( $data, 'get_fields_recursive' );
-//	}
-//
-//	return $data;
-//} );
-//
-//
-//
-///**
-// * Mystery function
-// */
-//function get_fields_recursive( $item ) {
-//	if ( is_object( $item ) ) {
-//		$item->acf = array();
-//		if ( $fields = get_fields( $item ) ) {
-//			$item->acf = $fields;
-//			array_walk_recursive( $item->acf, 'get_fields_recursive' );
-//		}
-//	}
-//}
+/**
+ * V3
+ */
+/**
+ * Define which post-types/taxonomies should get to pass along their ACF fields
+ *
+ * https://github.com/airesvsg/acf-to-rest-api/issues/109#issuecomment-286403063
+ */
+add_filter( 'acf/rest_api/page/get_fields', function ( $data ) {
+	if ( ! empty( $data ) ) {
+		array_walk_recursive( $data, 'get_fields_recursive' );
+	}
+
+	return $data;
+} );
+
+
+
+/**
+ * Function
+ */
+function get_fields_recursive( $item ) {
+	if ( is_object( $item ) ) {
+		$item->acf = array();
+		if ( $fields = get_fields( $item ) ) {
+			$item->acf = $fields;
+			array_walk_recursive( $item->acf, 'get_fields_recursive' );
+		}
+	}
+}
--- public/wp-content/plugins/fewbricks_definitions/field-groups/templates/post.php
+++ public/wp-content/plugins/fewbricks_definitions/field-groups/templates/post.php
@@ -1,4 +1,5 @@
 <?php
+
 use fewbricks\bricks AS bricks;
 use fewbricks\acf AS fewacf;
 use fewbricks\acf\fields AS acf_fields;
@@ -6,41 +7,23 @@ use fewbricks\acf\fields AS acf_fields;
 // --- Setting components on default page template ---
 
 $location = [
-    [
-        [
-            'param'    => 'post_type',
-            'operator' => '==',
-            'value'    => 'post'
-        ],
-	    [
-		    'param'    => 'page_template',
-		    'operator' => '!=',
-		    'value'    => 'page-templates/landing.php'
-	    ],
-	    [
-		    'param'    => 'page_template',
-		    'operator' => '!=',
-		    'value'    => 'page-templates/products-and-services.php'
-	    ],
-	    [
-		    'param'    => 'page_template',
-		    'operator' => '!=',
-		    'value'    => 'page-templates/sectors.php'
-	    ]
-    ]
+	[
+		[
+			'param'    => 'post_type',
+			'operator' => '==',
+			'value'    => 'post'
+		]
+	]
 ];
 
 
-
 $fg7 = ( new fewacf\field_group( 'Lead Text', '201903111530a', $location, 5, [
-    'position' => 'acf_after_title',
-    'names_of_items_to_hide_on_screen' => [
-    ]
-] ));
+	'position' => 'acf_after_title',
+] ) );
 
 $fg7->add_field( new acf_fields\text( 'Lead Text', 'post_lead_text', '201903111530b', [
-    'instructions' => 'Optionally enter some lead text for the page (maximum length, 200 characters)',
-    'maxlength' => 200
+	'instructions' => 'Optionally enter some lead text for the page (maximum length, 200 characters)',
+	'maxlength'    => 200
 ] ) );
 
 $fg7->register();

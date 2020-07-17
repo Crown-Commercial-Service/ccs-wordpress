3de1ac09c7c36e93238c07a250ddefacfc647c0b
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2020-07-17 11:45:16 +0100

    deleting lot on saleforce will now delete local data too

--- public/wp-content/plugins/ccs-salesforce/includes/cli-commands.php
+++ public/wp-content/plugins/ccs-salesforce/includes/cli-commands.php
@@ -277,18 +277,18 @@ class Import
         // Import this Framework
         $framework = $this->importSingleFramework($framework);
 
-        $this->updateFrameworkSearchIndexWithSingleFramework($framework);
+        // $this->updateFrameworkSearchIndexWithSingleFramework($framework);
 
-        // Mark whether a supplier has any live frameworks
-        $this->checkSupplierLiveFrameworks();
+        // // Mark whether a supplier has any live frameworks
+        // $this->checkSupplierLiveFrameworks();
 
-        $this->updateSupplierSearchIndex();
+        // $this->updateSupplierSearchIndex();
 
-        // Update framework titles in WordPress to include the RM number
-        $this->updateFrameworkTitleInWordpress();
+        // // Update framework titles in WordPress to include the RM number
+        // $this->updateFrameworkTitleInWordpress();
 
-        // Update lot titles in WordPress to include the RM number and the lot number
-        $this->updateLotTitleInWordpress();
+        // // Update lot titles in WordPress to include the RM number and the lot number
+        // $this->updateLotTitleInWordpress();
 
         $response = [
           'importCount' => $this->importCount,
@@ -545,6 +545,20 @@ class Import
 
             }
 
+        }
+
+        $localLot = $this->getLotsalesforceIdByframeworkId($framework->getSalesforceId());
+
+        $SFlotsSaleforceId = $this->extractSaleforceIdFromLots($lots);
+
+        foreach ($localLot as $key => $value){
+            if (in_array($key, $SFlotsSaleforceId) == false){
+
+                $lotWordPressId = $this->getLotWordpressIdBySalesforceId($key);
+
+                $this->deleteLot($key, $value);
+                $this->deleteWordpressLot($lotWordPressId);
+            }
         }
 
         return $framework;
@@ -1000,6 +1014,41 @@ class Import
         }
 
         return $lotWordpressId;
+    }
+
+    /**
+     * @param null|string $frameworkId
+     * @return null
+     */
+    protected function getLotsalesforceIdByframeworkId(?string $frameworkId)
+    {
+        $sql = "SELECT salesforce_id, lot_number FROM ccs_lots WHERE framework_id = '" . $frameworkId . "';";
+
+        $query = $this->dbConnection->connection->prepare($sql);
+        $query->execute();
+
+        $sqlData = $query->fetchAll(\PDO::FETCH_KEY_PAIR);
+
+        if (count($sqlData) == 0 ){
+            $sqlData = NULL;
+        }
+
+        return $sqlData;
+    }
+
+    /**
+     * @param $lots
+     * @return array
+     */
+    protected function extractSaleforceIdFromLots($lots)
+    {
+        $SaleforceIds = [];
+
+        foreach ($lots as $lot) {
+            $SaleforceIds[] = $lot->getSalesforceId();
+        }
+
+        return $SaleforceIds;
     }
 
 

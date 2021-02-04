946afcd0e7b0007f8efb2341cdc13d8d427cd7d7
Zubair Ali <zubair.ali@co-cc02842.ad.cabinetoffice.gov.uk>
2021-02-04 17:42:05 +0000

    seperate reindex code into another class

---
+++ src/App/Search/ReindexSearchClient.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Search;
+
+use App\Model\ModelInterface;
+use App\Search\Mapping\FrameworkMapping;
+use App\Search\Mapping\SupplierMapping;
+use App\Search\FrameworkSearchClient;
+use App\Search\SupplierSearchClient;
+use Elastica\Document;
+use Elastica\Mapping;
+use Elastica\Query;
+use Elastica\ResultSet;
+use Elastica\Search;
+use Elastica\Reindex;
+use Elastica\Client;
+use Elastica\Index;
+
+class ReindexSearchClient extends AbstractSearchClient
+{
+    protected $elasticaClient;
+
+    public function __construct()
+    {
+
+        // initialise frameworks and supplier
+        $this->frameworksClient = new FrameworkSearchClient();
+        $this->supplierClient = new SupplierSearchClient();
+
+        // config for elastica client
+        $config = [
+            'transport' => getenv('ELASTIC_TRANSPORT'),
+            'host'      => getenv('ELASTIC_HOST'),
+            'port'      => getenv('ELASTIC_PORT'),
+          ];
+          
+        // initialise Elastica client
+          $this->elasticaClient = new Client($config);
+    }
+
+
+
+    public function reindex () 
+    {
+        
+        $oldIndex = $this->elasticaClient->getIndex($this->frameworksClient->getQualifiedIndexName());
+        $newIndex = $this->elasticaClient->getIndex($this->frameworksClient->getQualifiedIndexName() . '_v1');
+
+        // create new temp index with new index settings to copy documents to
+        $this->createNewIndex($this->frameworksClient->getQualifiedIndexName() . '_v1');
+        
+        // copy documents from old index to new index
+        $reindexAPI = new Reindex($oldIndex, $newIndex);
+        $reindexAPI->run();
+
+        // delete old index
+        $oldIndex->delete();
+
+        // create new index with same name as old index
+        $this->createNewIndex($this->frameworksClient->getQualifiedIndexName());
+
+        // copy documents from new temp index to old index(with the new data and settings)
+        $reindexAPI = new Reindex($newIndex, $oldIndex);
+        $reindexAPI->run();
+        
+        // delete new temp index
+        $newIndex->delete();
+        
+        
+    }
+
+    public function createNewIndex (string $indexName) 
+    {
+        $index = $this->elasticaClient->getIndex($indexName);
+
+        $analysis = [
+          'analysis' => array(
+            'analyzer' => array(
+              'english_analyzer' => array(
+                'tokenizer' => 'standard',
+                'filter'    => array('lowercase', 'english_stemmer', 'english_stop'),
+              ),
+            ),
+            'filter'   => array(
+              'english_stemmer' => array(
+                'type' => 'stemmer',
+                'name' => 'english'
+              ),
+              'english_stop' => array(
+                'type' => 'stop',
+                'stopwords' => '_english_'
+              )
+            )
+          )
+        ];
+
+        $index->create(['settings' => $analysis]);
+
+        $index->setMapping($this->frameworksClient->getIndexMapping());
+    }
+}

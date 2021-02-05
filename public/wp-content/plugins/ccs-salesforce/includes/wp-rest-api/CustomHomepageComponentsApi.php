4998ccad9dd46183ccedd92f9fee5c81e360b1f1
Zubair Ali <zubair.ali@co-cc02842.ad.cabinetoffice.gov.uk>
2021-02-05 11:51:07 +0000

    resolve-conflicts

--- public/wp-content/plugins/ccs-salesforce/includes/wp-rest-api/CustomHomepageComponentsApi.php
+++ public/wp-content/plugins/ccs-salesforce/includes/wp-rest-api/CustomHomepageComponentsApi.php
@@ -33,13 +33,8 @@ class CustomHomepageComponentsApi
     public function get_homepage_components()
     {
         $howToBuy = [];
-<<<<<<< HEAD
-        if (have_rows('how_to_buy', 'option')):
-            while (have_rows('how_to_buy', 'option')): the_row();
-=======
         if (have_rows('hc_how_to_buy', 'option')):
             while (have_rows('hc_how_to_buy', 'option')): the_row();
->>>>>>> 3e3a4475b94e9b217b06773b14ff36ba9c8e9629
                 $howToBuy['title'] = get_sub_field('title');
                 $howToBuy['description'] = get_sub_field('description');
                 $howToBuy['link_text'] = get_sub_field('link_text');
@@ -49,13 +44,8 @@ class CustomHomepageComponentsApi
         endif;
 
         $agreements = [];
-<<<<<<< HEAD
-        if (have_rows('agreements', 'option')):
-            while (have_rows('agreements', 'option')): the_row();
-=======
         if (have_rows('hc_agreements', 'option')):
             while (have_rows('hc_agreements', 'option')): the_row();
->>>>>>> 3e3a4475b94e9b217b06773b14ff36ba9c8e9629
                 $agreements['title'] = get_sub_field('title');
                 $agreements['description'] = get_sub_field('description');
                 $agreements['link_text'] = get_sub_field('link_text');
@@ -65,13 +55,8 @@ class CustomHomepageComponentsApi
         endif;
 
         $upcomingDeals = [];
-<<<<<<< HEAD
-        if (have_rows('upcoming_deals', 'option')):
-            while (have_rows('upcoming_deals', 'option')): the_row();
-=======
         if (have_rows('hc_upcoming_deals', 'option')):
             while (have_rows('hc_upcoming_deals', 'option')): the_row();
->>>>>>> 3e3a4475b94e9b217b06773b14ff36ba9c8e9629
                 $upcomingDeals['title'] = get_sub_field('title');
                 $upcomingDeals['description'] = get_sub_field('description');
                 $upcomingDeals['link_text'] = get_sub_field('link_text');
@@ -81,26 +66,16 @@ class CustomHomepageComponentsApi
         endif;
 
         $catalogueTitle = [];
-<<<<<<< HEAD
-        if (have_rows('catalogue_title', 'option')):
-            while (have_rows('catalogue_title', 'option')): the_row();
-=======
         if (have_rows('hc_catalogue_title', 'option')):
             while (have_rows('hc_catalogue_title', 'option')): the_row();
->>>>>>> 3e3a4475b94e9b217b06773b14ff36ba9c8e9629
                 $catalogueTitle['title'] = get_sub_field('title');
             endwhile;
             
         endif;
 
         $catalogue1 = [];
-<<<<<<< HEAD
-        if (have_rows('catalogue1', 'option')):
-            while (have_rows('catalogue1', 'option')): the_row();
-=======
         if (have_rows('hc_catalogue1', 'option')):
             while (have_rows('hc_catalogue1', 'option')): the_row();
->>>>>>> 3e3a4475b94e9b217b06773b14ff36ba9c8e9629
                 $catalogue1['title'] = get_sub_field('title');
                 $catalogue1['link_url'] = get_sub_field('link');
                 $catalogue1['description'] = get_sub_field('description');
@@ -109,13 +84,8 @@ class CustomHomepageComponentsApi
         endif;
 
         $catalogue2 = [];
-<<<<<<< HEAD
-        if (have_rows('catalogue2', 'option')):
-            while (have_rows('catalogue2', 'option')): the_row();
-=======
         if (have_rows('hc_catalogue2', 'option')):
             while (have_rows('hc_catalogue2', 'option')): the_row();
->>>>>>> 3e3a4475b94e9b217b06773b14ff36ba9c8e9629
                 $catalogue2['title'] = get_sub_field('title');
                 $catalogue2['link_url'] = get_sub_field('link');
                 $catalogue2['description'] = get_sub_field('description');
@@ -124,13 +94,8 @@ class CustomHomepageComponentsApi
         endif;
 
         $catalogue3 = [];
-<<<<<<< HEAD
-        if (have_rows('catalogue3', 'option')):
-            while (have_rows('catalogue3', 'option')): the_row();
-=======
         if (have_rows('hc_catalogue3', 'option')):
             while (have_rows('hc_catalogue3', 'option')): the_row();
->>>>>>> 3e3a4475b94e9b217b06773b14ff36ba9c8e9629
                 $catalogue3['title'] = get_sub_field('title');
                 $catalogue3['link_url'] = get_sub_field('link');
                 $catalogue3['description'] = get_sub_field('description');
@@ -139,13 +104,8 @@ class CustomHomepageComponentsApi
         endif;
 
         $video= [];
-<<<<<<< HEAD
-        if (have_rows('video', 'option')):
-            while (have_rows('video', 'option')): the_row();
-=======
         if (have_rows('hc_video', 'option')):
             while (have_rows('hc_video', 'option')): the_row();
->>>>>>> 3e3a4475b94e9b217b06773b14ff36ba9c8e9629
                 $video['title'] = get_sub_field('title');
                 $video['video_link'] = get_sub_field('video_link');
                 $video['video_caption'] = get_sub_field('video_caption');

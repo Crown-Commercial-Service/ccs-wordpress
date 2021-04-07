798634312ab373dda27f38761da919577f091db4
Chee-Ng <chee.ng@crowncommercial.gov.uk>
2021-04-07 12:16:28 +0100

    changed deprecated dotenv constructor

--- public/search-api/frameworks.php
+++ public/search-api/frameworks.php
@@ -8,7 +8,7 @@ use Symfony\Component\Dotenv\Dotenv;
 $rootDir = __DIR__ . '/../../';
 
 require_once($rootDir . 'vendor/autoload.php');
-$dotenv = new Dotenv();
+$dotenv = new Dotenv(true);
 $dotenv->load($rootDir . '.env');
 
 $searchClient = new FrameworkSearchClient();

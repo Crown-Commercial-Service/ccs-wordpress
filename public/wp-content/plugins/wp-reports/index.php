1c4e589792646b07963f60e3a3b1ac04b9a53fb9
dorota-hawro <dorota.hawro@crowncommercial.gov.uk>
2021-09-06 10:07:57 +0100

    Updated api urls to use base url from .env

--- public/wp-content/plugins/wp-reports/index.php
+++ public/wp-content/plugins/wp-reports/index.php
@@ -12,11 +12,14 @@ if ( ! defined('ABSPATH')) exit; // exit if accessed directly
 
 class WpReportsPlugin {
 
-    private $authorsAPI = 'http://ccs-agreements.cabinetoffice.localhost/wp-json/wp-reports-plugin/v2/authors';
-    private $frameworksAPI = 'http://ccs-agreements.cabinetoffice.localhost/wp-json/wp-reports-plugin/v2/frameworks';
-    private $documentsAPI = "http://ccs-agreements.cabinetoffice.localhost/wp-json/wp-reports-plugin/v2/documents/type=frameworks";
+    private $authorsAPI;
+    private $frameworksAPI;
+    private $documentsAPI;
 
     function __construct() {
+        $authorsAPI = getenv('WP_SITEURL_WITH_HTTP') . '/wp-json/wp-reports-plugin/v2/authors';
+        $frameworksAPI = getenv('WP_SITEURL_WITH_HTTP') . '/wp-json/wp-reports-plugin/v2/frameworks';
+        $documentsAPI = getenv('WP_SITEURL_WITH_HTTP') . '/wp-json/wp-reports-plugin/v2/documents/type=frameworks';
         add_action('admin_menu', array($this, 'reportsMenu'));
         add_action('admin_init', array($this, 'reportsSettings'));
     }
--- public/wp-content/plugins/wp-reports/reports-api.php
+++ public/wp-content/plugins/wp-reports/reports-api.php
@@ -9,19 +9,22 @@ class ReportsApi {
     }
 
     function restApi () {
-        // http://ccs-agreements.cabinetoffice.localhost/wp-json/wp-reports-plugin/v2/authors
+        // DEV: https://webdev-cms.crowncommercial.gov.uk/wp-json/wp-reports-plugin/v2/authors
+        // LOCAL: http://ccs-agreements.cabinetoffice.localhost/wp-json/wp-reports-plugin/v2/authors
         register_rest_route( 'wp-reports-plugin/v2', 'authors', array(
             'methods' => 'GET',
             'callback' => array($this, 'authors')
         ) );
     
-        // http://ccs-agreements.cabinetoffice.localhost/wp-json/wp-reports-plugin/v2/frameworks
+        // DEV: https://webdev-cms.crowncommercial.gov.uk/wp-json/wp-reports-plugin/v2/frameworks
+        // LOCAL: http://ccs-agreements.cabinetoffice.localhost/wp-json/wp-reports-plugin/v2/frameworks
         register_rest_route( 'wp-reports-plugin/v2', 'frameworks', array(
             'methods' => 'GET',
             'callback' => array($this,'frameworks'),
         ) );
     
-        http://ccs-agreements.cabinetoffice.localhost/wp-json/wp-reports-plugin/v2/documents
+        // DEV: https://webdev-cms.crowncommercial.gov.uk/wp-json/wp-reports-plugin/v2/documents/type=frameworks
+        // LOCAL: http://ccs-agreements.cabinetoffice.localhost/wp-json/wp-reports-plugin/v2/documents
         register_rest_route( 'wp-reports-plugin/v2', 'documents/type=(?P<type>[a-zA-Z0-9-]+)', array(
             'methods' => 'GET',
             'callback' => array($this,'documents'),

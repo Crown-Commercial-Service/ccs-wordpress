71222f7406731e14e973b6f25d2a195ab30b21ca
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2024-05-23 12:47:34 +0100

    updated WP Last Modified Info plugin

--- public/wp-content/plugins/wp-last-modified-info/blocks/build/post-template-tag/index.asset.php
+++ public/wp-content/plugins/wp-last-modified-info/blocks/build/post-template-tag/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-server-side-render'), 'version' => '4d48ff766986732ad7cf');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '8ed05c9809a33a9f17be');

e5a212bf3d2bc743b5f6067b164d003b443590ca
zubairali6 <zubair.ali@crowncommercial.gov.uk>
2025-04-15 11:59:18 +0100

    Remove fuzzines for multiple fields

--- src/App/Search/FrameworkSearchClient.php
+++ src/App/Search/FrameworkSearchClient.php
@@ -151,8 +151,6 @@ class FrameworkSearchClient extends AbstractSearchClient implements SearchClient
             $multiMatchQuery = new Query\MultiMatch();
             $multiMatchQuery->setQuery($keyword);
             $multiMatchQuery->setFields(['description^2', 'summary', 'benefits', 'how_to_buy', 'keywords^2']);
-            $multiMatchQuery->setFuzziness(1);
-            $multiMatchQuery->setPrefixLength(3);
             $keywordBool->addShould($multiMatchQuery);
 
             // Add a boost to the title

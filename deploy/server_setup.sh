0c96b15afaf50b48a3b3222fe9425d1f0d396b89
Will Trust <will.trust@crowncommercial.gov.uk>
2025-08-28 17:19:52 +0100

    grouping logrotate

--- deploy/server_setup.sh
+++ deploy/server_setup.sh
@@ -40,12 +40,14 @@ if [ ! -e "$FIRST_RUN_PATH" ]; then
     sudo chown root:root \
         "$SCRIPTDIR/$DEPLOYMENT_TYPE/files/awscli.conf" \
         "$SCRIPTDIR/$DEPLOYMENT_TYPE/files/awslogs.conf" \
+        "$SCRIPTDIR/$DEPLOYMENT_TYPE/files/applogs" \
         "$SCRIPTDIR/$DEPLOYMENT_TYPE/logrotate.conf"
 
     echo "> > chmod'ing awslogs config files..."
     sudo chmod 640 \
         "$SCRIPTDIR/$DEPLOYMENT_TYPE/files/awscli.conf" \
         "$SCRIPTDIR/$DEPLOYMENT_TYPE/files/awslogs.conf" \
+        "$SCRIPTDIR/$DEPLOYMENT_TYPE/files/applogs" \
         "$SCRIPTDIR/$DEPLOYMENT_TYPE/files/logrotate.conf"
 
     echo "> > Movinging awslogs config files..."
@@ -54,10 +56,10 @@ if [ ! -e "$FIRST_RUN_PATH" ]; then
         "$SCRIPTDIR/$DEPLOYMENT_TYPE/files/awslogs.conf" \
         /etc/awslogs/
 
-    echo "> > Moving log rotate config file..."
+    echo "> > Moving log rotate config files..."
     sudo mv -f \
-        "$SCRIPTDIR/$DEPLOYMENT_TYPE/files/logrotate.conf" \
-        /etc/logrotate.conf
+        "$SCRIPTDIR/$DEPLOYMENT_TYPE/files/logrotate.conf" /etc/logrotate.conf
+        "$SCRIPTDIR/$DEPLOYMENT_TYPE/files/applogs" /etc/logrotate.d/
 
     echo "> > Moving cloudwatch rotate config file..."
     sudo mv -f \
@@ -105,17 +107,6 @@ if [ ! -e "$FIRST_RUN_PATH" ]; then
     sudo curl -s -o wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     sudo chmod +x wp
     sudo mv -f wp /usr/local/bin/
-
-    echo "> > chown'ing logrotate config files..."
-    sudo chown root:root "$SCRIPTDIR/$DEPLOYMENT_TYPE/files/applogs"
-
-    echo "> > chmod'ing logrotate config files..."
-    sudo chmod 644 "$SCRIPTDIR/$DEPLOYMENT_TYPE/files/applogs"
-
-    echo "> > Moving logrotate config files..."
-    sudo mv -f \
-        "$SCRIPTDIR/$DEPLOYMENT_TYPE/files/applogs" \
-        /etc/logrotate.d/
 
     echo "> > chown'ing php config file..."
     sudo chown root:root \

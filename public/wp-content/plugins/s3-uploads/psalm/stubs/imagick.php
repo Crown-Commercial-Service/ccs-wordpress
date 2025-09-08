58fd2e7da4f345fe5f1286e5958a9ebc783018d1
zubairali6 <zubair.ali@crowncommercial.gov.uk>
2025-09-08 10:33:17 +0100

    update wordpress plugins

--- public/wp-content/plugins/s3-uploads/psalm/stubs/imagick.php
+++ public/wp-content/plugins/s3-uploads/psalm/stubs/imagick.php
@@ -1,13 +1,11 @@
 <?php
 
-namespace {
-	/**
-	 * @method Imagick clone() (PECL imagick 2.0.0)<br/>Makes an exact copy of the Imagick object
-	 * @link https://php.net/manual/en/class.imagick.php
-	 */
-	class Imagick implements Iterator, Countable
-	{
-	}
+/**
+ * @method Imagick clone() (PECL imagick 2.0.0)<br/>Makes an exact copy of the Imagick object
+ * @link https://php.net/manual/en/class.imagick.php
+ */
+class Imagick implements Iterator, Countable {
+
 }
 
 namespace WP_CLI {
@@ -17,7 +15,7 @@ namespace WP_CLI {
 	 * @param callable|class-string $class
 	 * @return void
 	 */
-	function add_command(string $command, $class)
-	{
+	function add_command( string $command, $class ) {
+
 	}
 }

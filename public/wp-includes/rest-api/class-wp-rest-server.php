2d37bab6a3f499574c0b2c1ee6d2c6c996b17ffe
zubairali6 <zubair.ali@crowncommercial.gov.uk>
2025-11-25 12:30:57 +0000

    upgrade wordpress to 6.8.3

--- public/wp-includes/rest-api/class-wp-rest-server.php
+++ public/wp-includes/rest-api/class-wp-rest-server.php
@@ -1182,7 +1182,7 @@ class WP_REST_Server {
 					$args[ $param ] = $value;
 				}
 			}
-
+			// issue when upgrading wordpress so leave this code as it is now if wordpress tries to update it
 			foreach ( $handlers as $handler ) {
 				$callback = $handler['callback'];
 
@@ -1372,6 +1372,9 @@ class WP_REST_Server {
 			'home'            => home_url(),
 			'gmt_offset'      => get_option( 'gmt_offset' ),
 			'timezone_string' => get_option( 'timezone_string' ),
+			'page_for_posts'  => (int) get_option( 'page_for_posts' ),
+			'page_on_front'   => (int) get_option( 'page_on_front' ),
+			'show_on_front'   => get_option( 'show_on_front' ),
 			'namespaces'      => array_keys( $this->namespaces ),
 			'authentication'  => array(),
 			'routes'          => $this->get_data_for_routes( $this->get_routes(), $request['context'] ),
